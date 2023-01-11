11cf5fc4e7e843cb28032abfdfa18736ce773e3b
Corey Alix <CoreyAlix@hotmail.com>
2023-01-10 21:14:14 -0500

    Works on windows

--- html-to-pdf.js
+++ html-to-pdf.js
@@ -1,10 +1,10 @@
 import puppeteer from "puppeteer";
-const URL = process.argv[2] || "https://ca0v.us/resume.cv.html";
+const URL = process.argv[2] || "https://ca0v.us/resume/cv.html";
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: "/opt/homebrew/bin/chromium",
+    //executablePath: "/opt/homebrew/bin/chromium",
   });
   const page = await browser.newPage();
 
